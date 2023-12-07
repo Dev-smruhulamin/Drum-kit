f45e02290379cf4687e225d083384012ca017c40
Dev-smruhulamin <smruhulamin1993@gmail.com>
2023-12-07 21:44:37 +0600

    on key function added

--- js/index.js
+++ js/index.js
@@ -1,17 +1,8 @@
-function removeTransition(e) {
-    if (e.propertyName !== `transform`) return;
-    e.target.classList.remove(`playing`);
-}
-function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.code}"]`);
-    const key = document.querySelector(`div[data-key="${e.code}"]`);
-    if (!audio) return;
-
-    key.classList.add(`playing`);
-    audio.currentTime = 0;
+window.addEventListener(`keydown`, function(e){
+    const audio =document.querySelector(`audio[data-key="${e.keyCode}"]`); 
+    const key =document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return;// stop the function
+    audio.currentTime= 0; // time 
     audio.play();
-}
-
-const keys = Array.from(document.querySelectorAll(`.key`));
-keys.forEach(key => key.addEventListener(`transitionend`, removeTransition));
-window.addEventListener(`keydown`, playSound);
+    
+});
